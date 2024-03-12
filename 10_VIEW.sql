b098abc44d0a398a5139073822f7c544366c757f
SeonKyuKimm <ksg112809@gmail.com>
2024-03-12 17:45:08 +0900

    240312class

---
+++ 10_VIEW.sql
@@ -0,0 +1,231 @@
+/* VIEW
+  - SELECT 문의 실행결과 (RESULT SET)를 화면에 저장하는 객체
+  - 논리적 가상 테이블
+  --> 테이블 모양을 하고는 있지만, 실제로 값을 저장하고 있지는 않다.
+  
+  ** VIEW 사용목적 **
+  
+  1) 복잡한 SELECT문을 쉽게 재사용하기 위해 사용한다
+  
+  
+  2) 테이블의 진짜 모습을 감출 수 있어서 보안상 유리하다.
+  (신입사원들한테 보통 제공함. DB의 실제 값은 안줌)
+  
+  
+  ***** VIEW 사용 시 주의사항 *****
+  
+  1) 가상 테이블( 실제 테이블이 X ) --> ALTER 구문등은 사용 불가함
+  2) VIEW를 이용한 DML(INSERT / UPDATE / DELETE)이 
+  		가능한 경우도 있지만,
+  		많은 제약이 따르기 때문에 SELECT 용도로만 사용하는것을 권장한다
+  
+  		
+  [ VIEW 생성 방법 ]
+  
+ 	CREATE [OR REPLACE] [FORCE | NOFORCE]VIEW 뷰의이름 
+  AS SUBQUERY(만들고 싶은 뷰 모양의 SUBQUERY)
+  [WITH CHECK OPTION]
+  [WITH READ ONLY];
+  
+  -- 1) OR REPLACE : 기존에 동일한 뷰이름이 존재하는 경우 덮어쓰고,
+  																					존재하지 않으면 새로 생성한다.
+   
+ 	-- 2) FORCE | NOFORCE 
+ 				FORCE : 서브쿼리에 사용된 테이블이 존재하지 않아도 뷰 생성 
+ 				NOFORCE : 서브쿼리에 사용된 테이블이 반드시 존재해야만 뷰 생성 (얘가 기본값)
+
+	-- 3) WITH CHECH OPTION : 옵션을 설정한 컬럼의 값을 수정 불가능하게 한다. (DML 같은것 막을때 좀 사용함)
+
+
+	-- 4) WITH READ ONLY : 뷰에 대해 '조회(읽기.READ)'만 가능하게 함. (DML 수행 불가)												*/
+
+
+
+
+
+
+-- 사번, 이름, 부서명, 직급명 조회결과를 저장하는 VIEW 생성하기
+
+CREATE VIEW V_EMP
+AS SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME
+	 FROM EMPLOYEE
+	 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+	 JOIN JOB USING (JOB_CODE);
+	--ORA-01031 : 권한이 불충분하다.
+	-- 1) SYS 관리자 계정 접속
+	-- 2) ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+	-- 3) GRANT CREATE VIEW TO kh_ksk;
+	-- 4) 권한 부여 받은 kh 계정으로 다시 접속하여
+ 	--		위 VIEW 생성 구문 재실행하기
+	ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+	GRANT CREATE VIEW TO kh_ksk; -- 권한부여
+	
+
+ 
+-- 생성된 VIEW 를 이용하여 조회해보기
+SELECT * FROM V_EMP;
+
+
+/*────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ ───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────*/
+
+-- OR REPLACE 확인 + 별칭등록
+
+CREATE OR REPLACE VIEW V_EMP
+AS SELECT EMP_ID 사번, EMP_NAME 이름, DEPT_TITLE 부서명, JOB_NAME 직급명
+	 FROM EMPLOYEE
+	 JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID)
+	 JOIN JOB USING (JOB_CODE);
+
+--ORA-00955: 기존의 객체가 이름을 사용하고 있습니다.
+-- OR REPLACE 추가 후 실행 --> 성공
+	-- > RLWHS V_EMP를 새로운 VIEW 로 덮어쓰기한것.
+	
+SELECT * FROM V_EMP;
+
+
+
+/*────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ ───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────*/
+
+
+
+
+-- VIEW 를 이용한 DML 확인
+
+--테이블 복사
+CREATE TABLE DEPT_COPY2
+AS SELECT * FROM DEPARTMENT;
+
+SELECT * FROM DEPT_COPY2;
+
+
+-- 복사한 DEPT_COPY2 테이블을 이용해서 VIEW 생성
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID FROM DEPT_COPY2;
+
+
+-- 뷰 생성 확인
+SELECT * FROM V_DCOPY2;
+
+
+
+-- 뷰를 이용해서 INSERT 하기
+
+INSERT INTO V_DCOPY2 
+VALUES('D0','L3'); -- 1행 삽입됨
+
+SELECT * FROM V_DCOPY2; -- VIEW에 'D0'가 삽입된걸 확인함
+--> 가상의 테이블인 VIEW에 데이터 삽입이 가능한걸까 ? => NO!
+
+
+--> 원본 테이블 확인
+SELECT * FROM DEPT_COPY2;
+--> VIEW 에 삽입한 내용이 원본 테이블에 존재함.
+--> VIEW 를 이용한 DML구문이 원본에 영향을 미친다.
+
+
+-- VIEW 의 원래 목적인 보여지는것(조회용) 이라는 용도에 맞게 사용하는게 좋다.
+--> WITH READ ONLY옵션 사용
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID FROM DEPT_COPY2
+WITH READ ONLY; -- 읽기 전용 VIEW생성 (SELECT 만 가능)
+
+SELECT* FROM DEPT_COPY2;
+
+INSERT INTO V_DCOPY2 
+VALUES('D0','L3');
+--RA-42399: 읽기 전용 뷰에서는 DML 작업을 수행할 수 없습니다.
+
+
+SELECT * FROM V_DCOPY2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 11_SEQUENCE.sql
@@ -0,0 +1,236 @@
+/*
+		시퀀스 ( 순서, 연속, 수열 )
+	
+	- 순차적 번호 자동 발생기 역할의 객체
+	
+	-> SEQUENCE 객체를 이용해서 호출하게 되면 
+		 지정된 범위 내에서 일정한 간격으로 증가하는 숫자가
+		 순차적으로 출력된다
+
+
+
+EX ) 1 부터 10까지 1씩 증가하고 반복하는 시퀀스 객체
+
+1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10 ...
+
+*** SEQUENCE 는 주로 PRIMARY KEY 역할의 컬럼에 삽입되는 값을 만드는 용도로 사용 ***
+PRIMARY KEY = NOT NULL , UNIQUE 
+
+
+
+
+[ 작성법 ]
+
+
+CREATE SEQUENCE 시퀀스 이름(내가설정ㅎ)
+[START WITH 숫자] -- 처음 발생시킬 시작값 지정, 자동 1이 기본이다.
+[INCREMENT BY 숫자] -- 증가치. 다음 값에 대한 증가치. 몇씩 커지게 할건지. 생략하면 자동 1이 기본
+[MAXVALUE 숫자 | NOMAXVALUE] -- 발생시킬 최대값 지정 ( 최대 기본값 10의 27승 -1 )
+[MINVALUE 숫자 | NOMINVALUE] -- 최소값 지정 ( -10의 26승 )
+[CYCLE | NOCYCLE] -- 순환할건지 말건지, 값 여부 지정
+[CACHE 바이트크기 | NOCACHE] -- 캐시 메모리 기본값은 20BYTE, 최소값은 2BYTE
+
+
+
+
+
+-- 시퀀스의 캐시메모리는 할당된 크기만큼 미리 다음값들을 생성해 저장해둔다.
+
+--> 시퀀스 호출 시 미리 저장되어진 값들을 가져와 반환하므로 
+-- 매번 시퀀스를 생성해서 반환하는것보다 DB속도가 향상된다.
+
+
+
+*** 시퀀스 사용 방법 ***
+
+
+1 ) 시퀀스명.NEXTVAL : 다음 시퀀스 번호를 얻어옴. (INCREMENT BY 만큼 증가된 값)
+											단, 시퀀스 생성 후 첫 호출인 경우 START WITH의 값을 얻어옴.
+
+
+2 ) 시퀀스명.CURRVAL : 현재 시퀀스 번호를 얻어옴 .
+											 단, 시퀀스 생성 후 NEXTVAL 호출 없이 CURRVAL를 호출하면 오류 발생.
+																																																						*/
+
+-- 옵션 없이 SEQUENCE 생성
+-- 범위 : 1 ~ MAXVALUE
+-- 시작 :1
+-- 반복 X (NOCYCLE)
+-- CACHE MEMORY 20BYTE (기본값)
+CREATE SEQUENCE SEQ_TEST;
+
+
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;
+-- * CURRVAL 주의사항 * 
+--> 마지막 NEXTVAL 호출 값을 다시 보여주는 기능
+--> NEXTVAL를 먼저 호출해야 CURRVAL 호출이 가능하다!
+
+
+
+
+
+-- 생성 하자마자 바로 현재 값 확인
+SELECT SEQ_TEST.CURRVAL FROM DUAL;
+--ORA-08002: 시퀀스 SEQ_TEST.CURRVAL은 이 세션에서는 정의 되어 있지 않습니다
+
+SELECT SEQ_TEST.NEXTVAL FROM DUAL; --1 
+SELECT SEQ_TEST.CURRVAL AS 현재값 FROM DUAL; --1
+
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 2
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 3
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 4
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 5
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 6
+SELECT SEQ_TEST.NEXTVAL FROM DUAL;-- 7
+
+SELECT SEQ_TEST.CURRVAL AS 현재값 FROM DUAL; --7
+
+-- .NEXTVAL 사용 이후에 .CURRVAL 사용이 가능하다.
+
+
+/*────────────────────────────────────────────────────────────────────────────────────────────────────────
+────────────────────────────────────────────────────────────────────────────────────────────────────────*/
+
+
+-- 실제 사용 예시
+
+CREATE TABLE EMP_TEMP
+AS SELECT EMP_ID, EMP_NAME FROM EMPLOYEE;
+
+SELECT * FROM EMP_TEMP;
+
+-- 223번부터 10씩 증가하는 시퀀스 생성
+CREATE SEQUENCE SEQ_TEMP
+START WITH 223 -- 223시작
+INCREMENT BY 10 -- 10씩 증가
+NOCYCLE -- 반복 X 가 기본값
+NOCACHE; -- 캐시 X (CACHE 20 기본값)
+
+-- EMP_TEMP 테이블에 사원 정보 삽입
+
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'홍길동'); -- 223번으로 드가있음
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'고길동'); -- 233
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'김길동'); -- 243
+
+
+SELECT * FROM EMP_TEMP
+ORDER BY EMP_ID DESC;
+
+
+
+/*────────────────────────────────────────────────────────────────────────────────────────────────────────
+────────────────────────────────────────────────────────────────────────────────────────────────────────*/
+
+
+
+
+-- SEQUENCE 수정
+
+/*
+
+	
+	
+
+/*
+ALTER SEQUENCE 시퀀스 이름(내가설정ㅎ)
+[INCREMENT BY 숫자] -- 증가치. 다음 값에 대한 증가치. 몇씩 커지게 할건지. 생략하면 자동 1이 기본
+[MAXVALUE 숫자 | NOMAXVALUE] -- 발생시킬 최대값 지정 ( 최대 기본값 10의 27승 -1 )
+[MINVALUE 숫자 | NOMINVALUE] -- 최소값 지정 ( -10의 26승 )
+[CYCLE | NOCYCLE] -- 순환할건지 말건지, 값 여부 지정
+[CACHE 바이트크기 | NOCACHE] -- 캐시 메모리 기본값은 20BYTE, 최소값은 2BYTE
+*/
+
+
+
+-- SEQ.TEMP를 1씩 증가하는 형태로 변경하기
+ALTER SEQUENCE SEQ_TEMP
+INCREMENT BY 1;
+
+
+
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'이길동'); -- 223번으로 드가있음
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'박길동'); -- 233번으로 자동 들어감
+INSERT INTO EMP_TEMP VALUES(SEQ_TEMP.NEXTVAL,'최길동');-
+
+
+
+
+SELECT * 
+FROM EMP_TEMP
+ORDER BY 1 DESC;
+
+ -- EX )중복없는 고객번호 생성
+
+
+
+-- 테이블 뷰 시퀀스 삭제방법
+DROP TABLE EMP_TEMP;
+DROP VIEW V_DCOPY2;
+DROP SEQUENCE SEQ_TEMP;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
